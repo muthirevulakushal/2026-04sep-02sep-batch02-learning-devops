f1ac9ee58495c2b105c70523cc8b67e41ce1c278
muthirevulakushal <muthirevulakushal@gmail.com>
2026-09-05 04:20:43 +0530

    Update Kubernetes manifests

--- 00-sources/pythonapp/app.py
+++ 00-sources/pythonapp/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), 'templates'))
 
 @app.route("/")
 def home():
-    return "Hello from Docker + Kubernetes! for V11.0.....Release by Kumarans , DevOps Engg...04Sep2026 for Batch2.....You are AWSome"
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
