d6fba7685ae868030c471cc4ee05d087addac8e2
muthirevulakushal <muthirevulakushal@gmail.com>
2026-09-05 04:44:13 +0530

    updated app file3

--- 00-sources/pythonapp/app.py
+++ 00-sources/pythonapp/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Kushal from Docker + Kubernetes! for V03.0.....Release by Kushal , DevOps Engg...04Sep2026 for Batch2.....You are Awesome"
+    return "Hello from Docker + Kubernetes! for V11.0.....Release by Kumarans , DevOps Engg...04Sep2026 for Batch2.....You are AWSome"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
