b7ed5b5481b868fa7df4ba3e75a06c5d3649b1d8
muthirevulakushal <muthirevulakushal@gmail.com>
2026-09-05 03:55:21 +0530

    Fix template path for Kubernetes deployment

--- 00-sources/pythonapp/app.py
+++ 00-sources/pythonapp/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), 'templates'))
 
 @app.route("/")
 def home():
