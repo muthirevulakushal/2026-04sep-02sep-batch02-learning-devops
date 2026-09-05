f45b7217eda8495645de77d0d042593ef7b75aaa
muthirevulakushal <muthirevulakushal@gmail.com>
2026-09-06 00:20:27 +0530

    Update app.py

--- 00-sources/pythonapp/app.py
+++ 00-sources/pythonapp/app.py
@@ -1,10 +1,10 @@
+from flask import Flask
 
-from flask import Flask, render_template
-import os
-app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), 'templates'))
+app = Flask(__name__)
+
 @app.route("/")
 def home():
-    return render_template('home.html')cls
+    return "Hello Kushal !!! Docker + Kubernetes! for V12.0.....Release by Kushal , DevOps Engg...04Sep2026 for Batch2.....You are AWSome"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
